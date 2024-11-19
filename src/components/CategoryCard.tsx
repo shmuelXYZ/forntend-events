eb89ac2fa9da8fe2f79fb325194724f20e072b7a
Natanel Amar <natanelamar35@gmail.com>
2024-11-19 12:58:21 +0200

    delete the button tag and replace it with a <p></p> tag on the CategoryCard component

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -17,18 +17,13 @@ const CategoryCard: React.FC<CategoryCardProps> = ({ name }) => {
     <li
       onClick={() => handleClick()}
       value={name}
-      className={`bg-cover bg-center h-96 w-auto rounded-2xl mx-4 flex justify-around mb-4`}
+      className={`bg-cover bg-center h-96 w-auto rounded-2xl mx-4 flex justify-center items-center mb-4 `}
       style={{
         backgroundImage: `url(/src/assets/${name}.webp)`,
       }}
     >
-      <button
-        value={name}
-        type="button"
-        className="bg-gradient-to-r from-red-600 via-green-500 to-purple-600 text-transparent bg-clip-text opacity-80 text-5xl sm:text-6xl md:text-8xl lg:text-9xl font-extrabold "
-      >
-        {name}
-      </button>
+      <p className="bg-gradient-to-r from-red-600 via-green-500 to-purple-600 text-transparent bg-clip-text opacity-80 text-5xl sm:text-6xl md:text-8xl lg:text-9xl font-extrabold ">
+        {name}</p>
     </li>
   );
 };
